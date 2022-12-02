30927796ca94a1db753af1d0e97c24567136a350
xm-tech <maxm09@163.com>
2022-12-02 20:08:26 +0800

    Makefile & .gitignore

---
+++ Makefile
@@ -0,0 +1,7 @@
+build:
+	zip -r Chess.love *.lua assets
+run:
+	love Chess.love
+clean:
+	rm -f Chess.love
+
---
+++ main.lua
@@ -0,0 +1,9 @@
+local love = require "love"
+
+function love.load()
+
+end
+
+function love.draw()
+	love.graphics.rectangle("fill", 50, 50, 50, 60)
+end

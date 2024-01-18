a0cc1ef2ace055336419ceaf1fa9ad6b0690c4d1
xm-tech <maxm09@163.com>
2024-01-18 16:47:53 +0800

    optmize chess structure && fix log

--- chessboard.lua
+++ chessboard.lua
@@ -22,8 +22,8 @@ local function init_chesses()
 	-- 我方阵营(颜色), 0-红, 1-黑
 	local me = math.random(2)
 	-- 对方阵营
-	local oppo
-	if me == 1 then oppo = 2 else oppo = 1 end
+	local other
+	if me == 1 then other = 2 else other = 1 end
 	-- 棋子唯一标识
 	local id = 0
 	local chess = require "chessman"
@@ -32,11 +32,16 @@ local function init_chesses()
 		for _, data in pairs(g.chess_both[who]) do
 			-- data: {棋子类别，x, y, 名称}
 			local c = chess:new()
-			id = id + 1
-			print("id:", id, "cap:", _cap, "tid:", data[1], "x:", data[2], "y:", data[3], "name:", data[4])
-			local img = (_cap-1).."-"..data[1]..".png"
+			-- id = id + 1
+			id = data[1]
+			tid = data[2]
+			x = data[3]
+			y = data[4]
+			name = data[5]
+			print("id:", id, "cap:", _cap, "tid:", tid, "x:", x, "y:", y, "name:", name)
+			local img = (_cap-1).."-"..tid..".png"
 			-- id, tid, x, y, cap, img, alive, name
-			c:init(id, data[1], data[2], data[3], _cap, img, true, data[4])
+			c:init(id, tid, x, y, _cap, img, true, name)
 			c.image = g.love.graphics.newImage("assets/"..c.img)
 			M.chesses[c.id] = c
 			local grid = g.get_grid_idx(c.x, c.y)
@@ -47,7 +52,7 @@ local function init_chesses()
 	-- 初始化己方棋子
 	_init_chesses(1, me)
 	-- 初始化对方棋子
-	_init_chesses(2, oppo)
+	_init_chesses(2, other)
 end
 
 M.init = function ()
@@ -70,18 +75,20 @@ M.draw = function ()
 	end
 end
 
-M.mouse_pressed = function(x, y, button, istouch)
+M.mouse_pressed = function(x, y)
 	local grid = g.get_grid_idx(x, y)
 	local grid_chess = M.grid_chess_map[grid + 1]
 	local grid_chess_name = nil
+	local chess_cap = nil
 	if grid_chess then
 		grid_chess:detail()
+		chess_cap = grid_chess.cap
 		grid_chess_name = grid_chess.name
 		M.chess_selected = grid_chess
 	else
 		grid_chess_name = "no"
 	end
-	print("mousepressed, button:", button, "istouch:", istouch, "x:", x, "y:", y, "grid:", grid, "chess_name:", grid_chess_name, "cap:", grid_chess.cap)
+	print("mousepressed,x:", x, "y:", y, "grid:", grid, "chess_name:", grid_chess_name, "cap:", chess_cap)
 end
 
 return M
